d2af90d2c5ed61a1f7aa381588515e7972305d7e
1190201104 <631450560@qq.com>
2021-05-15 17:23:34 +0800

    3

--- src/P1/MagicSquare.java
+++ src/P1/MagicSquare.java
@@ -6,7 +6,7 @@ public class MagicSquare {
 	public static boolean generateMagicSquare(int n) {
 		if(n<0 || n%2==0)
 		{
-			System.out.print("输入不符合要求，请输入正奇数");
+			System.out.println("输入不符合要求，请输入正奇数");
 			return false;
 		}
 		int magic[][] = new int[n][n];//根据输入的数字生成数组
@@ -29,37 +29,35 @@ public class MagicSquare {
 				else
 					col++;//若不是就转到下一列
 			} 
-		}
-		for (i = 0; i < n; i++) 
-		{
-			for (j = 0; j < n; j++)
-				System.out.print(magic[i][j] + "\t");
-		System.out.println();
-		}
+		}		
+		File txt=new File("src/P1/txt/6.txt");
 		try {
-			OutputStreamWriter fout = null;//定义一个流 。
-			fout = new OutputStreamWriter(new FileOutputStream("D:\\JAVA\\sourse\\Lab1-1190201104\\src\\P1\\txt\\6.txt"),"GBK");
-//			FileOutputStream fout = new FileOutputStream("D:\\JAVA\\sourse\\Lab1-1190201104\\src\\txt\\txt.6");
-//			OutputStreamWriter writer = new OutputStreamWriter(fout);
-			for (i = 0; i < n; i++) 
+			txt.createNewFile();
+			FileWriter writer = new FileWriter(txt);
+			BufferedWriter out = new BufferedWriter(writer);
+//			OutputStreamWriter fout = null;//定义一个流 。
+//			fout = new OutputStreamWriter(new FileOutputStream("D:\\JAVA\\sourse\\Lab1-1190201104\\src\\P1\\txt\\6.txt"),"GBK");
+//			BufferedWriter buffWriter = new BufferedWriter(fout);			
+			for(i=0;i<n;i++)
 			{
 				for (j = 0; j < n; j++)
-					{
-					fout.write(magout[i][j]);
-					fout.write("\t");
-					}
-				fout.write("\n");
-			}
-			fout.close();
+				{
+					out.write(magout[i][j]);
+					out.write("\t");
+				}
+				out.write("\n");
+			}	
+			out.close();
 		} catch (Exception e) {		
 			e.printStackTrace();
 		} 
+		
 		return true;
 		}
 	public static boolean isLegalMagicSquare(String fileName)
 	{
 		boolean ret=true;
-		String []s=new String [150];
+		String []s=new String [200];
 		   int i=0;
 		   int j=0;
 		   try//按行读入字符串，s中存放的是每一行的内容
@@ -80,48 +78,51 @@ public class MagicSquare {
 		 
 		    int length=i;//length记录行数
 		    //用width记录每一行的数字个数
-		    int width0[]=new int[length];
+		    int width[]=new int[length];
 		    for(i=0;i<length;i++)
 		    {   
-			  width0[i]=(s[i].split("\t")).length;
-			  if(length != width0[i] )
-			  {   
+			  width[i]=(s[i].split("\t")).length;
+			  if(length != width[i] )
+			  {   if(length==(s[i].split(" ")).length)
+				     System.out.print("存在两数未用\t分隔，");
+			      else
+			      {
 				  if(i>0)//若开始几行行列相等，后面不相等，那肯定不是矩阵
-					  System.out.print("并非矩阵");
-				  else if(i==0 && width0[0]==s[1].split("\t").length)
-					  System.out.print("行列数不相等");//如果一开始就不相等那就不是正方形
+					  System.out.print("某行与其他行不对齐，不是矩阵，");
+				  else if(i==0 && width[0]==s[1].split("\t").length)
+					  System.out.print("行列数不相等，");//如果一开始就不相等那就不是正方形
+			      }
 				  ret=false;
 				  return ret;
 			  }
 			  
 		    }
-		    int width=width0[0];//如果运行到这里那肯定是行列数量相等	
-		    
-		    String [][]ss= new String[length][width];
-		    int [][]num = new int[length][width];
+		    //如果运行到这里那肯定是行列数量相等			    
+		    String [][]ss= new String[length][length];
+		    int [][]num = new int[length][length];
 		    for(i=0;s[i]!=null;i++) 
 		    {
 			  ss[i]=s[i].split("\t");//用二维字符串数组记录数据
 		    }		  		  
 		    for(i=0;i<length;i++)
-		    {   for(j=0;j<width;j++)
+		    {   for(j=0;j<length;j++)
 	      		{  
 		    	   if(ss[i][j].contains(" ")==true)
 		    	   { 
-		    		 System.out.print("存在空格");
+		    		 System.out.print("数字中存在空格，");
 		    	   	 ret=false;
 					 return ret;  
 		    	   }
 		    	   else if(ss[i][j].contains(".")==true)
 		    	   {
-		    		   System.out.print("存在非int型整数");
+		    		   System.out.print("存在非int型整数，");
 			    	   ret=false;
 					   return ret; 
 		    	   }		    		   
 	    	       num[i][j]=Integer.valueOf(ss[i][j]);//把字符串转化为数字
 	    	       if(num[i][j]<0)
 	    	       {   
-	    	    	   System.out.print("存在负数");
+	    	    	   System.out.print("存在负数，");
 	    	    	   ret=false;
 	 				   return ret;
 	    	       }
@@ -148,10 +149,10 @@ public class MagicSquare {
 	    	  if(sum0!=sum && sum1!=sum)
 	    	  {
 	    		  ret=false;
-	    		  System.out.print("和不满足");
+	    		  System.out.print("行或列的和不全相等，");
 	    		  break;
 	    	  }
-		    }
+		    }		    
 		    //对角比较
 		    sum1=0;
 		    sum0=0;
@@ -162,25 +163,29 @@ public class MagicSquare {
 		    }
 		    if(sum0!=sum && sum1 !=sum)
 		    {   
-	    	  System.out.print("和不满足");
+	    	  System.out.print("对角线上的和不全相等，");
 	    	  ret=false;
 		    }	      
 		    return ret;
 	}
 	public static void main(String[] args)   {
-		String txt[]= {"1.txt","2.txt","3.txt","4.txt","5.txt",};
+		String txt[]= {"1.txt","2.txt","3.txt","4.txt","5.txt","6.txt"};
 		int num=txt.length;
 		int n;
 		boolean equal;//把名字存在数组中，轮流放入判断
+		Scanner in = new Scanner(System.in);
+		n=in.nextInt();
+		generateMagicSquare(n);		
 		for(int i=0;i<num;i++)
 		{
 			System.out.print("第"+(i+1)+"个矩阵：");
 		    equal=isLegalMagicSquare(txt[i]);
-			System.out.println(equal);
+			if(equal==true)
+				System.out.println("是Magic Square");
+			else
+				System.out.println("不满足Magic Square条件");
 		}
-		Scanner in = new Scanner(System.in);
-		n=in.nextInt();
-		generateMagicSquare(n);
+		
 	   }    
 }
 
